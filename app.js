632a151b3dae2342fc51b2cd83ea14d9cd37f518
sourabh003 <csourabh003@gmail.com>
2024-03-05 02:27:06 +0530

    updated

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const app = express();
 const { decode } = require("url-encode-decode");
 const fs = require("fs");
 const convert = require("./convert");
+const path = require("path");
 
 app.get("/", (_, res) => res.json({ success: true }));
 
 app.get("/convert", async (req, res) => {
 	const { url } = req.query;
 
-	let outputFilePath = "tmp/" + Date.now() + ".mp3";
+	let outputFilePath = path.join(process.cwd(), `tmp/${Date.now()}.mp3`);
 	let decodedUrl = decode(url);
 
 	await convert(decodedUrl, outputFilePath);
