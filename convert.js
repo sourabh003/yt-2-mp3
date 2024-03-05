e8f66e497dc9fcd03f91ade50f76c1d530a84de2
sourabh003 <csourabh003@gmail.com>
2024-03-06 01:42:10 +0530

    final commit

--- convert.js
+++ convert.js
@@ -1,19 +1,11 @@
 const youtubedl = require("youtube-dl-exec");
 const ffmpeg = require("fluent-ffmpeg");
-const axios = require("axios");
+const fs = require("fs");
 
 module.exports = async (videoUrl, outputFilePath) => {
 	try {
 		// Download the video using youtube-dl-exec
-		// await youtubedl(videoUrl, { o: outputFilePath, f: "bestaudio[ext=m4a]" });
-		const response = await axios.get(videoUrl, { responseType: "stream" });
-
-		await new Promise((resolve, reject) => {
-			const writeStream = fs.createWriteStream(outputFilePath);
-			response.data.pipe(writeStream);
-			writeStream.on("finish", resolve);
-			writeStream.on("error", reject);
-		});
+		await youtubedl(videoUrl, { o: outputFilePath, f: "bestaudio[ext=m4a]" });
 
 		// Convert the downloaded video to MP3 using ffmpeg
 		await new Promise((resolve, reject) => {
@@ -21,7 +13,7 @@ module.exports = async (videoUrl, outputFilePath) => {
 				.input(outputFilePath)
 				.audioCodec("libmp3lame")
 				.audioBitrate(320)
-				.save("output.mp3")
+				.save(outputFilePath)
 				.on("end", () => {
 					console.log("Conversion finished.");
 					// Optionally, delete the original downloaded file
